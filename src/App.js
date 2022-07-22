720d2a4a8715c6c8b575156a52ab49f2f61c3f8f
Aaron Philip <aaronphilip2003@gmail.com>
2022-07-22 22:01:22 +0530

    Basic Website to demonstrate use of State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react';
 
 function App() {
+  const [call,setCall]=React.useState()
+
+  function answer(){
+    setCall(prevState=>!prevState)
+  }  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gradient-to-r from-lime-500 border-2 border-black h-80 w-80 rounded-xl mx-96 mt-40 p-10'>
+      <div className='ml-20 font-bold'>McDonalds?</div>
+      <div className='my-20 mx-20 border-black border-2 rounded-full p-6 bg-blue-500 hover:bg-blue-700 text-white font-bold hover:cursor-pointer' onClick={answer}>{call?"YES":"NO"}</div>
     </div>
   );
 }
